3616c48813a1dc641692bac59feabc89ac67722b
Kyle Ward <warddk@pbworld.com>
2016-06-06 14:26:11 -0400

    Remove the machine-specific ct-ramp batch files

--- generic/programs/CTRampEnv - ORIGINAL.bat
+++
@@ -1,8 +0,0 @@
-rem this file has environment variables for CT-RAMP batch files
-
-rem set location of java
-set JAVA_64_PATH=C:\Progra~1\Java\jdk1.7.0
-set TRANSCAD_PATH=C:\Progra~1\TransCAD 6.0
-
-rem set location of GNU tools
-set GNUWIN32_PATH=c:\progra~2\GnuWin32\bin
